78ff1b4e4d1aa429b0a716667917dbb6c25670f4
Emily Morgado <emilyannemorgado@gmail.com>
2019-03-20 08:11:10 -0700

    Work on text bubble styles. Add more conversation dummy data

--- components/TextBubble.js
+++ components/TextBubble.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, Text, View, StyleSheet } from 'react-native';
+import { FlatList, Text, ScrollView, StyleSheet, View } from 'react-native';
 
 // STYLES
 const styles = StyleSheet.create({
@@ -8,17 +8,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     width: '100%',
     height: '100%',
+    overflow: 'scroll',
   },
   basicBubble: {
+    // justifyContent: 'space-evenly',
+    justifyContent: 'center',
     borderStyle: 'solid',
     borderWidth: 3,
     borderRadius: 15,
-    fontSize: 22,
-    // lineHeight: 3,
-    padding: '1%',
+    fontSize: 20,
+    padding: '3%',
+    lineHeight: 0,
     flexDirection: 'column',
     marginBottom: 15,
     maxWidth: '65%',
+    // overflow: 'visible',
   },
   bot: {
     borderColor: '#C34271',
@@ -27,8 +31,7 @@ const styles = StyleSheet.create({
   user: {
     borderColor: '#12C988',
     backgroundColor: '#16FFBD',
-    right: 1,
-    // marginRight: 5,
+    alignSelf: 'flex-end',
   },
 });
 
@@ -37,11 +40,15 @@ const TextBubble = props => (
   <View style={styles.viewContainer}>
     <FlatList
       data={[
-        {key: 'Devin', text: 'I am the robot!!!', from: 'bot'},
-        {key: 'Emily', text: 'I am so angry!', from: 'user'},
-        {key: 'Jackson', text: 'Are you really?', from: 'bot'},
-        {key: 'James', text: 'Yes!', from: 'bot'},
-        {key: 'Joel', text: 'I love validating others.', from: 'bot'},
+        {key: '1a', text: 'I am so angry!', from: 'user'},
+        {key: '2a', text: 'Why would you do this to me? I thought you loved me! But you hurt me. You abandonded me!', from: 'user'},
+        {key: '3a', text: "I'm so sorry", from: 'bot'},
+        {key: '4a', text: "I don't want you to hurt.", from: 'bot'},
+        {key: '5a', text: "I really let you down.", from: 'bot'},
+        {key: '6a', text: "You're the worst. Why would you do this to me?", from: 'user'},
+        {key: '7a', text: "You're right.", from: 'bot'},
+        {key: '8a', text: "I don't know. I wish I could take it back.", from: 'bot'},
+        {key: '9a', text: "You didn't deserve that.", from: 'bot'},
       ]}
       renderItem={({item}) => <Text style={[styles.basicBubble, styles[item.from]]}>{item.text}</Text>}
     />
